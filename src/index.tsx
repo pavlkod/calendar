1be58989756d4dea799a26ffe8aca5abfb879bd3
Pavel.K <pavelweb74@gmail.com>
2021-09-09 21:03:57 +0500

    Create structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/pages/login.ts
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const Login:FC = (props) => {
+    return 'Login'
+}
+
+export default Login
