9f1152f33e52d6507800f5ae83fd9a84d70cffb1
Pavel.K <pavelweb74@gmail.com>
2021-09-12 13:31:30 +0500

    Connect redux store

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.render(
-  <Provider store={}>
+  <Provider store={store}>
     <Router>
       <React.StrictMode>
         <App />
