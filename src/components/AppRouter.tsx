04ba3ef2f93e450667e798fc1d940a8d6c99751b
Pavel.K <pavelweb74@gmail.com>
2021-09-15 23:00:02 +0500

    Add auth reducer

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
-
+import { useTypedSelector } from '../hooks/useTypedSelector'
 import { privateRoutes, publicRoutes, RouteNames} from '../router'
 
 interface Props {
@@ -8,17 +7,17 @@ interface Props {
 }
 
 const AppRouter = (props: Props) => {
-    const auth = true
+    const auth = useTypedSelector(state => state.auth.isAuth)
     return (
         auth
             ?
             <Switch>
-                {privateRoutes.map(route => <Route {...route} />)}
+                {privateRoutes.map(route => <Route key={route.path} {...route} />)}
                 <Redirect to={ RouteNames.EVENTS}/>
             </Switch>
             :
             <Switch>
-                {publicRoutes.map(route => <Route {...route} />)}
+                {publicRoutes.map(route => <Route key={route.path}  {...route} />)}
                 <Redirect to={ RouteNames.LOGIN}/>
             </Switch>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,24 @@
 import { Menu} from 'antd'
 import { Header } from 'antd/lib/layout/layout'
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
+import { useTypedSelector } from '../hooks/useTypedSelector'
+import { RouteNames } from '../router'
 
-const Navbar:FC = () => {
+const Navbar: FC = () => {
+    const auth = useTypedSelector(state => state.auth.isAuth)
     return (
         <Header >
-            <Menu theme="dark" mode="horizontal" style={{justifyContent:'end'}}>
-                <Menu.Item key="1">Login</Menu.Item>
+            <Menu theme="dark" mode="horizontal" style={{ justifyContent: 'end' }}>
+                {auth
+                    ?
+                        <>
+                           <Menu.Item key="1"> <div>User</div></Menu.Item>
+                            <Menu.Item key="2"><Link to={RouteNames.LOGIN}>Выйти</Link></Menu.Item>
+                        </>
+                    :
+                        <Menu.Item key="1"><Link to={RouteNames.LOGIN}>Login</Link></Menu.Item>
+                }
             </Menu>
         </Header>
     )
