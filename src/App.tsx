2ca95f53383834bf0a47eab903db529876fa5507
Pavel.K <pavelweb74@gmail.com>
2021-09-24 22:43:45 +0500

    Add get auth from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Layout } from 'antd';
 import { Content } from 'antd/lib/layout/layout';
 
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/IUser';
 
-const App:FC = () => {
+const App: FC = () => {
+  const {setUser, setIsAuth } = useActions()
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setIsAuth(true)
+      setUser({username: localStorage.getItem('username')} as IUser)
+    } else {
+      setIsAuth(false)
+      setUser({} as IUser)
+    }
+  }, [])
   return (
     <Layout>
       <Navbar />
