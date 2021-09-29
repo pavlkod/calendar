dd92d5eb7b6f63a078d1d5e99ed379d1a4a08cd3
Pavel.K <pavelweb74@gmail.com>
2021-09-29 23:18:41 +0500

    Add modal

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,13 +1,25 @@
-import { Layout, Row } from 'antd'
-import React, { FC } from 'react'
+import { Button, Layout, Modal, Row } from 'antd'
+import React, { FC, useState } from 'react'
 import EventCalendar from '../components/EventCalendar'
 
-const Events:FC = () => {
+const Events: FC = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    const showModal = () => setIsModalVisible(true)
+    const handleOk = () => setIsModalVisible(false)
+    const handleCancel = () => setIsModalVisible(false)
     return (
         <Layout>
             <Row justify="center" align="middle" className="height-100">
-                <EventCalendar/>
+                <EventCalendar events={[] }/>
             </Row>
+            <Row justify="center">
+                <Button onClick={showModal}>Добавить событие</Button>
+            </Row>
+            <Modal title="Добавить событие" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
         </Layout>
     )
 }
