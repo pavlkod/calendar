ec8a932fa89d0311a05a44ace05f52dbe5457b6f
Pavel.K <pavelweb74@gmail.com>
2021-09-29 22:45:05 +0500

    Add interface for props in component EventCalendar

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,7 +1,12 @@
 import { Calendar } from 'antd'
-import React from 'react'
+import React, { FC } from 'react'
+import { IEvent } from '../models/IEvent'
 
-const EventCalendar = () => {
+interface EventCalendarProps{
+    events: IEvent[]
+}
+
+const EventCalendar:FC<EventCalendarProps> = () => {
     return (
         <Calendar/>
     )
