487bcdae38a95319bb14c3b9209c3cd881b77f71
Pavel.K <pavelweb74@gmail.com>
2021-10-04 22:32:07 +0500

    Collect event info from form

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -1,38 +1,60 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Form, Input, Button, DatePicker, Row, Select } from 'antd';
 import { rules } from '../utils/rules';
+import { IUser } from '../models/IUser';
+import { IEvent } from '../models/IEvent';
+import { Moment } from 'moment';
+import { formatDate } from '../utils/date';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const { Option } = Select;
 
-const EventForm:FC = () => {
+interface EventProps{
+    guests: IUser[],
+    submit: (event:IEvent) => void
+}
+
+const EventForm: FC<EventProps> = (props) => {
+    const [event, setEvent] = useState<IEvent>({
+        author: '',
+        description: '',
+        date: '',
+        guest: ''
+    } as IEvent)
+    const selectGuest = (guest:string) => setEvent({...event, guest})
+    const selectDate = (date: Moment | null) => {
+        if (date) {
+            setEvent({...event, date: formatDate(date.toDate())})
+        }
+    }
+    const {user } = useTypedSelector(state => state.auth)
+    const formSubmit = () => {
+        props.submit({ ...event, author: user.username });
+        
+    }
     return (
-        <Form>
+        <Form onFinish={formSubmit}>
             <Form.Item
                 label="Описание события"
                 name="description"
                 rules={[rules.required('Введите описание события!')]}
             >
-                <Input />
+                <Input value={event.description} onChange={ (e) => setEvent({...event, description: e.target.value}) }/>
             </Form.Item>
             <Form.Item
                 label="Дата события"
                 name="date"
                 rules={[rules.required('Введите дату события!')]}
             >
-                <DatePicker style={{width:'100%'}}/>
+                <DatePicker style={{ width: '100%' }} onChange={selectDate}/>
             </Form.Item>
             <Form.Item
-                label="Пользователь"
+                label="Выберите гостя"
                 name="user"
-                rules={[rules.required('Введите пользователя!')]}
+                rules={[rules.required('Введите имя гостя!')]}
             >
-                <Select >
-                    <Option value="jack">Jack</Option>
-                    <Option value="lucy">Lucy</Option>
-                    <Option value="disabled" disabled>
-                        Disabled
-                    </Option>
-                    <Option value="Yiminghe">yiminghe</Option>
+                <Select onChange={selectGuest}>
+                    {props.guests.map((guest, i) => <Option key={i} value={guest.username}>{guest.username}</Option>)}
                 </Select>
             </Form.Item>
             <Row justify="end">
